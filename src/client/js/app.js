cb2fd3dc1fc88eef88ef28f32ecdcb10b7be7635
Diana <diana.mukaliyeva@gmail.com>
2020-06-06 17:15:33 +0300

    Implement endpoints on server side

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -55,31 +55,52 @@ export const getImage = async (url='', cityData, api_key) => {
     }
 }
 
+const getTripInformation = async (url, data) => {
+    const request = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    try {
+        const data = await request.json();
+        return data;
+    } catch (error) {
+        return ({error, error});
+    }
+}
+
 export const handleSubmit = async (event) => {
     event.preventDefault();
-    const data = [];
-    const username = 'mdiana';
-    const weather_key = 'd24718d636654782a2825a08f03f3c96';
-    const pixabay_key = '16892068-6efdba9e3cc819c3fd622d1fe';
-
+    // const username = 'mdiana';
+    // const weather_key = 'd24718d636654782a2825a08f03f3c96';
+    // const pixabay_key = '16892068-6efdba9e3cc819c3fd622d1fe';
+    
     const city = document.getElementById('city').value;
     const date = document.getElementById('depart_date').value;
+    const now = new Date();
+    const timeOffset = now.getTimezoneOffset() * 60000;
+    const data = {city: city, departDate: date, timeOffset: timeOffset};
     console.log(city);
     console.log(date);
-    try {
-        const cityData = await getCityDetail('http://api.geonames.org/searchJSON?maxRows=1&', city, username);
-        console.log(cityData);
-        postCityDetail('http://localhost:5000/cityDetails', cityData);
-        const weatherData = await getWeather('https://api.weatherbit.io/v2.0/current?image_type=photo&', cityData.geonameId, weather_key);
-        console.log(weatherData);
-        postWeatherData('http://localhost:5000/weatherData', weatherData.data[0]);
-        const imgUrl = await getImage('https://pixabay.com/api/?', cityData, pixabay_key);
-        console.log(imgUrl);
-        postImageData('http://localhost:5000/images', imgUrl);
-        fetch('http://localhost:5000/data')
-        .then (res => {return res.json()})
-        .then(res => console.log(res));
-    } catch (e) {
-        alert('Something went wrong');
-    }
+    const tripInfo = await getTripInformation('http://localhost:5000/tripInfo', data);
+    console.log(tripInfo);
+    // try {
+    //     const cityData = await getCityDetail('http://api.geonames.org/searchJSON?maxRows=1&', city, username);
+    //     console.log(cityData);
+    //     postCityDetail('http://localhost:5000/cityDetails', cityData);
+    //     const weatherData = await getWeather('https://api.weatherbit.io/v2.0/current?image_type=photo&', cityData.geonameId, weather_key);
+    //     console.log(weatherData);
+    //     postWeatherData('http://localhost:5000/weatherData', weatherData.data[0]);
+    //     const imgUrl = await getImage('https://pixabay.com/api/?', cityData, pixabay_key);
+    //     console.log(imgUrl);
+    //     postImageData('http://localhost:5000/images', imgUrl);
+    //     fetch('http://localhost:5000/data')
+    //     .then (res => {return res.json()})
+    //     .then(res => console.log(res));
+    // } catch (e) {
+    //     alert('Something went wrong');
+    // }
 };
--- src/server/server.js
+++ src/server/server.js
@@ -3,13 +3,14 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const data = [];
+const fetch = require('node-fetch');
+let data = {};
 
 /* Set up */
 
 // Setup dotenv
-// const dotenv = require('dotenv');
-// dotenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 // Set up an instance of app
 const app = express();
@@ -32,25 +33,74 @@ app.get('/', (req, res) => {
     res.sendFile('dist.html');
 })
 
-app.post('/cityDetails', (req, res) => {
-    data.push(req.body);
-    // console.log(data);
+app.post('/tripInfo', async (req, res) => {
+    try {
+        const city = req.body.city;
+        const timeOffset = req.body.timeOffset;
+        const date = new Date(req.body.departDate);
+        data.departDate = date.getTime();
+        const dateNow = new Date().setHours(0,0,0,0);
+        const oneDay = 60 * 60 * 1000 * 24;
+        const diff = data.departDate - dateNow + timeOffset;
+        let url = '';
+        if (diff > oneDay * 15) {
+            url = `https://api.weatherbit.io/v2.0/history/daily?start_date=${date.getFullYear()-1}-${date.getMonth()+1}-${date.getDate()}&end_date=${date.getFullYear()-1}-${date.getMonth()}-${date.getDate()+1}`
+        } else {
+            url = `http://api.weatherbit.io/v2.0/forecast/daily?`
+        }
+        await getCityDetail(city, process.env.GEONAME_USER);
+        await getForecast(url, process.env.WEATHER_KEY, data.cityId, diff);
+        await getPicture(process.env.PIXABAY_KEY);
+        res.json({success: 'success'});
+        // res.json(data);
+    } catch (error) {
+        res.json({error: 'error'});
+    }
 })
 
-app.post('/weatherData', (req, res) => {
-    console.log(req.body);
-    data.push(req.body);
-    console.log(data);
-})
+const getCityDetail = async (city, user) => {
+    const request = await fetch(`http://api.geonames.org/searchJSON?maxRows=1&q=${city}&username=${user}`);
+    const res = await request.json();
+    data.city = res.geonames[0].name;
+    data.country = res.geonames[0].countryName;
+    data.cityId = res.geonames[0].geonameId;
+}
 
-app.post('/images', (req, res) => {
-    console.log(req.body);
-    data.push(req.body);
-    console.log(data);
-})
+const getForecast = async (url, userKey, cityId, diff) => {
+    const request = await fetch(`${url}&city_id=${cityId}&key=${userKey}`);
+    try {
+        const res = await request.json();
+        const days = diff/1000/60/60/24;
+        if (days < 16) {
+            // console.log(res.data[days]);
+            data.weather = res.data[days];
+        } else {
+            // console.log(res.data[0]);
+            data.weather = res.data[0];
+        }
+    } catch (error) {
+        // console.log('no data');
+        data.weather = null;
+    }
+}
+
+const getPicture = async (key) => {
+    const request = await fetch(`https://pixabay.com/api/?image_type=photo&key=${key}&q=${data.city}+${data.country}`);
+    try {
+        const res = await request.json();
+        if (res.totalHits < 1) {
+            // console.log('no heats');
+            data.imageUrl = 'https://cdn.pixabay.com/photo/2016/01/19/15/48/luggage-1149289_1280.jpg';
+        } else {
+            // console.log(res.hits[0]);
+            data.imageUrl = res.hits[0].webformatURL;
+        }
+    } catch (error) {
+        data.imageUrl = 'https://cdn.pixabay.com/photo/2016/01/19/15/48/luggage-1149289_1280.jpg';
+    }
+}
 
 app.get('/data', (req, res) => {
-    // res.send(data);
     console.log(data);
-    res.json(data[0]);
+    res.json(data);
 })

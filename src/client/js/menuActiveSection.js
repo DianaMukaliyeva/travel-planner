3bae7a89dcb34903cebc320b1ecefec6dd8c27a8
Diana <diana.mukaliyeva@gmail.com>
2020-06-07 01:50:01 +0300

    Fix navigation active class

--- src/client/js/menuActiveSection.js
+++ src/client/js/menuActiveSection.js
@@ -2,17 +2,13 @@ const setActiveNavMenu = () => {
     let mainNavLinks = document.querySelectorAll('.menu__link');
     let fromTop = window.scrollY;
     let footer_nav = document.getElementById('footer_nav');
+    mainNavLinks.forEach(link => { link.classList.remove("active-class"); });
     if ((window.innerHeight + window.pageYOffset + 10) >= document.body.offsetHeight) {
-        mainNavLinks.forEach(link => { link.classList.remove("active-class"); });
         footer_nav.classList.add("active-class");
     } else if (window.scrollY <= window.innerHeight / 2) {
-        mainNavLinks.forEach(link => { link.classList.remove("active-class"); });
         document.getElementById('main_nav').classList.add("active-class");
     } else {
-        mainNavLinks.forEach(link => {
-            mainNavLinks.forEach(link => { link.classList.remove("active-class"); });
-            document.getElementById('trip_nav').classList.add("active-class");
-        });
+        document.getElementById('trip_nav').classList.add("active-class");
     }
 };
 
